d38b2562183d189a81909f74eeb4fc914ed36334
Gary <617864963@qq.com>
2023-09-14 11:11:05 +0800

    Second Commit

--- app/src/main/java/com/example/experiment2/MainActivity.java
+++ app/src/main/java/com/example/experiment2/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.experiment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main); //使用layout
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams params =  new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);  //设置布局中的控件居中显示
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        TextView textView = new TextView(this);                       //创建TextView控件
+        ImageView imageView = new ImageView(this);                    //创建 ImageView 控件
+        textView.setText(R.string.Hello_World);                     //设置TextView的文字内容
+        textView.setTextColor(Color.GREEN);                                  //设置TextView的文字颜色
+        textView.setTextSize(18);                                                //设置TextView的文字大小
+        imageView.setImageResource(R.drawable.my_image);
+        relativeLayout.addView(textView, params);                  //添加TextView对象和TextView的布局属性
+        relativeLayout.setLayoutParams(layoutParams);
+        setContentView(relativeLayout);                                  //设置在Activity中显示RelativeLayout
+        setContentView(imageView);                                       // 将 ImageView 添加到布局中
     }
 }
