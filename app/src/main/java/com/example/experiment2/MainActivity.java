c5f1339ebed107205b40b52298e33d98da5f031e
Gary <617864963@qq.com>
2023-09-14 13:27:08 +0800

    Second Commit

--- app/src/main/java/com/example/experiment2/MainActivity.java
+++ app/src/main/java/com/example/experiment2/MainActivity.java
@@ -20,21 +20,27 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout.LayoutParams params =  new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);  //设置布局中的控件居中显示
+        params.addRule(RelativeLayout.ALIGN_PARENT_TOP);  //设置布局中的控件置顶显示
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL); //设置布局中的控件居中显示
+        params.leftMargin = 100;  // X轴位置，单位为像素
+        params.topMargin = 800;   // Y轴位置，单位为像素
 
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM); //设置imageview控件布局
+
         TextView textView = new TextView(this);                       //创建TextView控件
         ImageView imageView = new ImageView(this);                    //创建 ImageView 控件
+
         textView.setText(R.string.Hello_World);                     //设置TextView的文字内容
         textView.setTextColor(Color.GREEN);                                  //设置TextView的文字颜色
         textView.setTextSize(18);                                                //设置TextView的文字大小
         imageView.setImageResource(R.drawable.my_image);
+
         relativeLayout.addView(textView, params);                  //添加TextView对象和TextView的布局属性
-        relativeLayout.setLayoutParams(layoutParams);
+        relativeLayout.addView(imageView,layoutParams);
+
         setContentView(relativeLayout);                                  //设置在Activity中显示RelativeLayout
-        setContentView(imageView);                                       // 将 ImageView 添加到布局中
     }
 }
