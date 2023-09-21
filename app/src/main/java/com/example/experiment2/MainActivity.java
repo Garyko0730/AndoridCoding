828df0b32eaa2880a11141267970804326171d12
Cooooooooold <kopang0730@stu2021.jnu.edu.cn>
2023-09-21 13:16:53 +0800

    fourth Commit

--- app/src/main/java/com/example/experiment2/MainActivity.java
+++ app/src/main/java/com/example/experiment2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.experiment2;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +16,14 @@ import android.widget.TextView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.security.PrivateKey;
+
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main); //使用layout
@@ -26,9 +33,11 @@ public class MainActivity extends AppCompatActivity {
         //交换文字
         Button button=findViewById(R.id.button_change);
         button.setText(R.string.button_1);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Button clickedButton = (Button) view;
                 TextView text_hello = findViewById(R.id.text_View_1);
                 TextView text_jnu = findViewById(R.id.text_View_2);
@@ -38,9 +47,33 @@ public class MainActivity extends AppCompatActivity {
                 text_jnu.setText(text1);
                 text_hello.setText(text2);
 
-                Toast.makeText(this,"交换成功",Toast.LENGTH_LONG.show());
-            }
-        });
+                Toast.makeText(MainActivity.this,"交换成功", Toast.LENGTH_SHORT).show();
+                showDialog(MainActivity.this, "", "交换成功啦！");
+                };
+            });
+        };
+        private void showDialog(MainActivity context, String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("我知道了", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        // 在确定按钮点击时执行的逻辑
+                    }
+                })
+
+                /*//不需要取消按钮，这里先注释掉了
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 在取消按钮点击时执行的逻辑
+                    }
+                })
+                //*/
+
+                .show();
+
 //交换文字
 
         //相对布局
